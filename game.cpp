c213c992836445932f8f8f6a89e487a966fd8c94
WhiteWyvern <bossi.luism@gmail.com>
2018-07-30 11:40:30 +0000

    game.cpp

--- game.cpp
+++ game.cpp
@@ -4,16 +4,18 @@
 //Quando for inicializar o personagem com uma função isso estará dentro dessa função.
 void returnLife (Tpersonagem* personagem, int life)
 {
+	personagem->lifeMAX = life;
 	personagem->life = life;
-	personagem->dano = 0;
 }
 
 
 //Namespace do programa.
+#define n 10 //Nao esquecer de fazer o mapa de tamanho variavel e lido de um arquivo.
 int main(int argc, char const *argv[])
 {
+	int tamBoard = n*n;
 	char move;
-	Tboard*	board = new Tboard[sizeof(Tboard)];
+	Tboard* board = new Tboard[sizeof(struct Tboard) + int(tamBoard)];
 	Tpersonagem* player = &(board->player);
 	Tpersonagem* enemy  = &(board->enemies[0]);
 	board->constroiBoard(board);
